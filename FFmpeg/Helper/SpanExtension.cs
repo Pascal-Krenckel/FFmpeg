98b7d3bb9ef0a147ea31f99de55e91d93757287c
Pascal-Krenckel <81245422+Pascal-Krenckel@users.noreply.github.com>
2026-07-28 17:09:04 +0200

    string.Join extension added

--- FFmpeg/Helper/SpanExtension.cs
+++ FFmpeg/Helper/SpanExtension.cs
@@ -1,4 +1,6 @@
-﻿namespace FFmpeg.Helper;
+﻿using System.Text;
+
+namespace FFmpeg.Helper;
 
 internal static class SpanExtension
 {
@@ -34,4 +36,59 @@ internal static class SpanExtension
     public static T? FirstOrDefault<T>(this Span<T> o) => o.FirstOrDefault(default);
     public static T? FirstOrDefault<T>(this Span<T> o, T? @default) => o.IsEmpty ? @default : o[0];
 
+    extension(string)
+    {
+        public static string Join<T>(char separator, ReadOnlySpan<T> span)
+        {
+            if (span.IsEmpty)
+                return string.Empty;
+            if (span.Length == 1)
+                return span[0]!.ToString();
+            StringBuilder sb = new();
+            _ = sb.Append(span[0]!.ToString());
+            for (int i = 1; i < span.Length; i++)
+                _ = sb.Append(separator).Append(span[i]!.ToString());
+            return sb.ToString();
+        }
+
+        public static string Join<T>(string separator, ReadOnlySpan<T> span)
+        {
+            if (span.IsEmpty)
+                return string.Empty;
+            if (span.Length == 1)
+                return span[0]!.ToString();
+            StringBuilder sb = new();
+            _ = sb.Append(span[0]!.ToString());
+            for (int i = 1; i < span.Length; i++)
+                _ = sb.Append(separator).Append(span[i]!.ToString());
+            return sb.ToString();
+        }
+
+        public static string Join<T>(char separator, ReadOnlySpan<T> span, Func<T,string> toString)
+        {
+            if (span.IsEmpty)
+                return string.Empty;
+            if (span.Length == 1)
+                return toString(span[0]);
+            StringBuilder sb = new();
+            _ = sb.Append(toString(span[0]));
+            for (int i = 1; i < span.Length; i++)
+                _ = sb.Append(separator).Append(toString( span[i]));
+            return sb.ToString();
+        }
+
+        public static string Join<T>(string separator, ReadOnlySpan<T> span, Func<T, string> toString)
+        {
+            if (span.IsEmpty)
+                return string.Empty;
+            if (span.Length == 1)
+                return toString(span[0]);
+            StringBuilder sb = new();
+            _ = sb.Append(toString(span[0]));
+            for (int i = 1; i < span.Length; i++)
+                _ = sb.Append(separator).Append(toString(span[i]));
+            return sb.ToString();
+        }
+
+    }
 }
