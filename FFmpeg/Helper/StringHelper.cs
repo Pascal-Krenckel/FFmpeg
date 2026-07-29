a9fc94aae88e51164027516e4930af9e87db6c9a
Pascal-Krenckel <81245422+Pascal-Krenckel@users.noreply.github.com>
2026-07-30 00:41:35 +0200

    Helper function string.Join(...,IReadOnlyList, Selector) added

--- FFmpeg/Helper/StringHelper.cs
+++ FFmpeg/Helper/StringHelper.cs
@@ -1,4 +1,6 @@
-﻿namespace FFmpeg.Helper;
+﻿using System.Text;
+
+namespace FFmpeg.Helper;
 
 internal static unsafe class StringHelper
 {
@@ -9,6 +11,36 @@ internal static unsafe class StringHelper
         while (*p++ != 0)
             i++;
         return i;
+    }
+
+    extension(string)
+    {     
+        public static string Join<T>(char separator, IReadOnlyList<T> span, Func<T, string> toString)
+        {
+            if (span.Count == 0)
+                return string.Empty;
+            if (span.Count == 1)
+                return toString(span[0]);
+            StringBuilder sb = new();
+            _ = sb.Append(toString(span[0]));
+            for (int i = 1; i < span.Count; i++)
+                _ = sb.Append(separator).Append(toString(span[i]));
+            return sb.ToString();
+        }
+
+        public static string Join<T>(string separator, IReadOnlyList<T> span, Func<T, string> toString)
+        {
+            if (span.Count == 0)
+                return string.Empty;
+            if (span.Count == 1)
+                return toString(span[0]);
+            StringBuilder sb = new();
+            _ = sb.Append(toString(span[0]));
+            for (int i = 1; i < span.Count; i++)
+                _ = sb.Append(separator).Append(toString(span[i]));
+            return sb.ToString();
+        }
+
     }
 
 }
