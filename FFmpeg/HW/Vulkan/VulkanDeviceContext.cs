943f08db8e046961c51d30fa442acd94a1b865c6
Pascal-Krenckel <81245422+Pascal-Krenckel@users.noreply.github.com>
2026-08-01 02:41:52 +0200

    Added vulkan device and frames context

---
+++ FFmpeg/HW/Vulkan/VulkanDeviceContext.cs
@@ -0,0 +1,44 @@
+﻿using FFmpeg.AutoGen;
+using FFmpeg.Unsafe;
+using FFmpeg.Utils;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FFmpeg.HW.Vulkan;
+
+/// <summary>
+/// Represents a Vulkan device context associated with an FFmpeg hardware device.
+/// </summary>
+public unsafe readonly struct VulkanDeviceContext : IAVPointer<_AVVulkanDeviceContext>
+{
+    readonly _AVVulkanDeviceContext* context;
+
+    internal VulkanDeviceContext(_AVVulkanDeviceContext* context)
+    {
+        this.context = context;
+    }
+
+    unsafe _AVVulkanDeviceContext* IAVPointer<_AVVulkanDeviceContext>.Pointer => context;
+
+    /// <summary>
+    /// Creates a <see cref="VulkanDeviceContext"/> from the hardware device associated with the specified frame.
+    /// </summary>
+    /// <param name="frame">
+    /// The frame whose hardware device context will be used.
+    /// </param>
+    /// <returns>
+    /// A <see cref="VulkanDeviceContext"/> representing the Vulkan device associated with the frame.
+    /// </returns>
+    /// <exception cref="InvalidOperationException">
+    /// Thrown if the frame does not have an associated hardware device context, or if the
+    /// associated device is not a Vulkan device.
+    /// </exception>
+    public static VulkanDeviceContext FromAVFrame(AVFrame frame)
+    {
+        if (frame.FramesContext.IsEmpty || frame.FramesContext.DeviceContext.DeviceType != DeviceType.Vulkan)
+            throw new InvalidOperationException();
+
+        return frame.FramesContext.DeviceContext.AsVulkan();
+    }
+}
---
+++ FFmpeg/HW/Vulkan/VulkanFramesContext.cs
@@ -0,0 +1,46 @@
+﻿using FFmpeg.AutoGen;
+using FFmpeg.Unsafe;
+using FFmpeg.Utils;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FFmpeg.HW.Vulkan;
+
+/// <summary>
+/// Represents a Vulkan hardware frames context.
+/// </summary>
+public readonly unsafe struct VulkanFramesContext : IAVPointer<_AVVulkanFramesContext>
+{
+    readonly internal _AVVulkanFramesContext* context;
+
+    internal VulkanFramesContext(_AVVulkanFramesContext* context)
+    {
+        this.context = context;
+    }
+
+    unsafe _AVVulkanFramesContext* IAVPointer<_AVVulkanFramesContext>.Pointer => context;
+
+    /// <summary>
+    /// Creates a <see cref="VulkanFramesContext"/> from the hardware frames context
+    /// associated with the specified frame.
+    /// </summary>
+    /// <param name="frame">
+    /// The frame whose hardware frames context will be used.
+    /// </param>
+    /// <returns>
+    /// A <see cref="VulkanFramesContext"/> representing the Vulkan hardware frames
+    /// context associated with the frame.
+    /// </returns>
+    /// <exception cref="InvalidOperationException">
+    /// Thrown if the frame does not have an associated hardware frames context, or if
+    /// the associated device is not a Vulkan device.
+    /// </exception>
+    public static VulkanFramesContext FromAVFrame(AVFrame frame)
+    {
+        if (frame.FramesContext.IsEmpty || frame.FramesContext.DeviceContext.DeviceType != DeviceType.Vulkan)
+            throw new InvalidOperationException();
+
+        return frame.FramesContext.AsVulkan();
+    }
+}
