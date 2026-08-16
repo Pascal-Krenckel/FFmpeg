cb3b002cb597581a508c7e4d610dfdeac4902879
Pascal-Krenckel <81245422+Pascal-Krenckel@users.noreply.github.com>
2026-08-16 15:06:26 +0200

    Code cleaning

--- FFmpeg/Threading/AwaitEventHandlerExtension.cs
+++ FFmpeg/Threading/AwaitEventHandlerExtension.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace FFmpeg.Threading;
+﻿namespace FFmpeg.Threading;
 
 /// <summary>
 /// Provides extension methods for asynchronously invoking event handlers.
