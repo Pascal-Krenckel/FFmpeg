5428836fe483c2be1f45a5330e65cd01f95ce7a1
Pascal-Krenckel <81245422+Pascal-Krenckel@users.noreply.github.com>
2026-09-02 01:56:35 +0200

    Test Logging

---
+++ FFmpegTest/LoggingTest.cs
@@ -0,0 +1,43 @@
+﻿using FFmpeg.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FFmpegTest;
+
+[TestClass]
+public class LoggingTest
+{
+    [TestInitialize]
+    public void Initialize()
+    {
+        FFmpeg.FFmpegLoader.Initialize();
+    }
+
+    [TestMethod]
+    public void TestLogging()
+    {
+
+        StringWriter sw = new();
+        FFmpeg.Logging.Logger.Level = FFmpeg.Logging.LogLevel.Debug;
+        FFmpeg.Logging.Logger.LogMessageReceived += LogMessage;
+        Logger.WriteLine(FFmpeg.Logging.LogLevel.Debug, "Test message Debug");
+        Logger.WriteLine(FFmpeg.Logging.LogLevel.Info, "Test message Info");
+        Logger.WriteLine(FFmpeg.Logging.LogLevel.Trace, "Test message Trace");
+        FFmpeg.Logging.Logger.LogMessageReceived -= LogMessage;
+
+        Assert.HasCount(2, messages);
+        Assert.AreEqual(FFmpeg.Logging.LogLevel.Debug, messages[0].Item1);
+        Assert.AreEqual("Test message Debug\n", messages[0].Item2);
+        Assert.AreEqual(FFmpeg.Logging.LogLevel.Info, messages[1].Item1);
+        Assert.AreEqual("Test message Info\n", messages[1].Item2);
+      
+    }
+    private List<(LogLevel, string)> messages = [];
+    private void LogMessage(string message, LogLevel level, ClassCategory category, string? contextName)
+    {
+        messages.Add((level, message));
+    }
+}
