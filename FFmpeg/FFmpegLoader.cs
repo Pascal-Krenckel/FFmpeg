a7eb11fea4a2a0b41fb00d9feff2f1bf63ec0025
Pascal-Krenckel <81245422+Pascal-Krenckel@users.noreply.github.com>
2026-09-02 01:57:53 +0200

    Update description: "\0" now stops further library detection.

--- FFmpeg/FFmpegLoader.cs
+++ FFmpeg/FFmpegLoader.cs
@@ -9,11 +9,12 @@ public static class FFmpegLoader
     /// <summary>
     /// Initializes the bindings for ffmpeg.
     /// </summary>
-    /// <param name="dir">The directory where the libraries are located. Use ';' as seperator for multiple directories. Use `null` to search for it.</param>
+    /// <param name="dir">The directory where the libraries are located. Use ';' as seperator for multiple directories. To disable auto search pass "\0" as a directory string.</param>
     /// <param name="throwIfFunctionNotFound">Whether an exception should be thrown when calling one of the functions couldn't be loaded.</param>
     /// <example>
-    /// The following code searches AppContext.BaseDirectory/(ffmpeg/(win-x64|linux-arm32...)) and %PATH% <c>Initialize();</c>
-    /// The following code specifies different dirs <c>Initialize("dir1;dir2")</c>
+    /// The following code searches AppContext.BaseDirectory/(ffmpeg/(win-x64|linux-arm32...)) and %PATH% <c>Initialize();</c> <br />
+    /// The following code specifies different dirs <c>Initialize("dir1;dir2")</c> <br />
+    /// The following code specifies different dirs and disables auto search default dirs <c>Initialize("dir1;dir2;\0")</c> <br />
     /// </example>
 
     public static void Initialize(string? dir = null, bool throwIfFunctionNotFound = true)
